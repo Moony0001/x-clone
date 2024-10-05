6eb5157cfeaac53475d1af81cce8f3d8ff213213
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2024-10-06 01:49:57 +0530

    2- UI Design Completed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+import daisyui from "daisyui";
+import daisyUIThemes from "daisyui/src/theming/themes";
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
+
+	daisyui: {
+		themes: [
+			"light",
+			{
+				black: {
+					...daisyUIThemes["black"],
+					primary: "rgb(29, 155, 240)",   //This is the primary color twitter blue
+					secondary: "rgb(24, 24, 24)",   //This is the secondary color black
+				},
+			},
+		],
+	},
+};
