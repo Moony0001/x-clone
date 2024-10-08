04ec115d78a17ec0c11c683dae43409bf80da3df
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2024-10-08 11:53:30 +0530

    3- fetch posts for the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/auth/login/LoginPage.jsx";
 import SignUpPage from "./pages/auth/signup/SignUpPage.jsx";
-import HomePage from "./pages/home/homepage.jsx";
+import HomePage from "./pages/home/HomePage.jsx";
 import Sidebar from "./components/common/Sidebar.jsx";
 import RightPanel from "./components/common/RightPanel.jsx";
 import NotificationPage from "./pages/notification/NotificationPage.jsx";
