ed64d381187c9bf47f3d01e325912ad534e1a564
SIDDHANT ROOPAK BAISWAR <111843248+Moony0001@users.noreply.github.com>
2024-10-16 16:33:39 +0530

    4- Prepared the app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
@@ -22,6 +23,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: "5mb"})); //to parse req.body the limit is to limit the size of the data that can be sent
 app.use(express.urlencoded({extended: true})); //to parse form data (urlencoded)
@@ -31,6 +33,14 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications",notificationRoutes);   //Don't forget to add the "/" before the api path
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));    //This is to serve the static files in the frontend folder
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 
 app.listen(PORT, ()=>{
